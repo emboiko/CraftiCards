5db90c4f3441cd134abf66098700b46c91fa5be1
Edward Boiko <Emboiko@gmail.com>
2020-07-13 08:55:24 -0400

    heroku config

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
@@ -15,6 +16,9 @@ app.use(userRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
 
 removeExpiredRSVPs();
