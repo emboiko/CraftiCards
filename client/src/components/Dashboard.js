52b1bf9dcb496948ab2103c757ed3d31fe37dd32
Emboiko <emboiko@gmail.com>
2020-07-15 19:10:31 -0400

    inital dashboard scaffold

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import Loading from "./utils/Loading";
+import axios from "axios";
 
 export default class Dashboard extends Component {
-  componentDidMount = () => {
+  constructor(props) {
+    super(props);
+    this.state = { rsvps: [], loading: true };
+  }
+
+  componentDidMount = async () => {
     document.title = "CraftiCards | Dashboard";
+    const req = await axios.get("/rsvps");
+    if (req.status === 200) this.setState({ rsvps: req.data.rsvps, loading: false });
   }
 
   render() {
-    return (
-      <div className="container">
-        <h1>Dashboard</h1>
-      </div>
-    );
+    if (this.state.loading) {
+      return (
+        <div className="container">
+          <Loading />
+        </div>
+      );
+    } else {
+      const rsvps = this.state.rsvps.map((rsvp, i) => {
+        return (
+          <Link to={`/cc/${rsvp.id}`} key={i}>{rsvp.title}</Link>
+        );
+      })
+
+      return (
+        <div className="container">
+          <h1 className="center">Dashboard</h1>
+          {rsvps}
+        </div>
+      );
+    }
   }
 }
